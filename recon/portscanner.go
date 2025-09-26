0b07ceaaff3da0fc90b1a14cb7fdee47230e34a2
flaskbush <flaskbush@gmail.com>
2025-09-27 07:31:47 +1100

    updated scanner for modularity

--- recon/portscanner.go
+++ recon/portscanner.go
@@ -3,37 +3,59 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
-const target = "192.168.88.112"
+type PortScanner struct{}
 
-func scanPort(target string, port int, wg *sync.WaitGroup, results chan<- string) {
-	defer wg.Done()
-	address := fmt.Sprintf("%s:%d", target, port)
-	conn, err := net.DialTimeout("tcp4", address, 2*time.Second)
-	if err == nil {
-		results <- fmt.Sprintf("Port %d ouvert", port)
-		conn.Close()
-	}
+func NewPortScanner() *PortScanner {
+	return &PortScanner{}
 }
 
-func main() {
+func (ps *PortScanner) Scan(target, portRange string) ([]string, error) {
 	var wg sync.WaitGroup
 	results := make(chan string)
+	var openPorts []string
 
 	go func() {
-		for result := range results {
-			fmt.Println(result)
+		for port := range results {
+			openPorts = append(openPorts, port)
 		}
 	}()
 
-	for port := 1; port <= 1024; port++ {
+	ports := parsePortRange(portRange)
+	for _, port := range ports {
 		wg.Add(1)
-		go scanPort(target, port, &wg, results)
+		go func(p int) {
+			defer wg.Done()
+			address := fmt.Sprintf("%s:%d", target, p)
+			conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+			if err == nil {
+				results <- fmt.Sprintf("Port %d open", p)
+				conn.Close()
+			}
+		}(port)
 	}
 
 	wg.Wait()
 	close(results)
+	return openPorts, nil
+
+}
+
+func parsePortRange(portRange string) []int {
+	var ports []int
+	parts := strings.Split(portRange, "-")
+	start, _ := strconv.Atoi(parts[0])
+	end := start
+	if len(parts) > 1 {
+		end, _ = strconv.Atoi(parts[1])
+	}
+	for i := start; i <= end; i++ {
+		ports = append(ports, i)
+	}
+	return ports
 }
